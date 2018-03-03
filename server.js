36363fbcb21437a7398bc17d8bd625b73fcd3051
Bino Varghese <binovarghese85@gmail.com>
2018-03-03 15:34:31 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+var app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+// set templete engine.views is the default directory that express uses for templates
+app.set('view engine', 'hbs')
+
+app.use((req, res, next) => {
+  var now = new Date().toString()
+  var log = `${now}: ${req.method} ${req.url}`
+  console.log(log)
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log')
+    }
+  })
+  next()
+})
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs')
+// })
+
+// to serve static
+app.use(express.static(__dirname + '/public'))
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase()
+})
+
+app.get('/', (req, res) => {
+  //res.send('<h1>Hello express!</h1>')
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMsg: 'Welcome to my website'
+  })
+})
+
+app.get('/about', (req, res) => {
+//  res.send('About page')
+  res.render('about.hbs', {
+    pageTitle: 'About page :)'
+  })
+})
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to fulfill request'
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server is up and running on port 3000')
+})
